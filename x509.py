9a141d9196df6f6e1ca309b34ea58ec3d9aae53e
Carlos Serrao <800109+pontocom@users.noreply.github.com>
2022-07-25 23:40:13 +0100

    Added the some more touches on x509 manipulation.

--- x509.py
+++ x509.py
@@ -14,6 +14,7 @@ def print_x509_menu():
     print("[1] Create Key Pair")
     print("[2] Create Self Signed Certificate")
     print("[3] Create CSR")
+    print("[4] Load CSR and Issue Certificate")
     print("[0] Exit")
 
 
@@ -37,6 +38,18 @@ def read_key_pair(filename):
     return private_key
 
 
+def read_csr(filename):
+    with open(filename, "rb") as f:
+        csr = f.read()
+    return csr
+
+
+def read_crt(filename):
+    with open(filename, "rb") as f:
+        cert = f.read()
+    return cert
+
+
 def do_x509_stuff():
     while True:
         print_x509_menu()
@@ -51,6 +64,11 @@ def do_x509_stuff():
             create_key_pair("user.key")
             privkey = read_key_pair("user.key")
             create_CSR(privkey)
+        elif option == '4':
+            privkey = read_key_pair("ca.key")
+            cert = read_crt("root_certificate.pem")
+            csr = read_csr("user.csr")
+            load_csr_and_issue_certificate(privkey, cert, csr)
         elif option == '0':
             break
         else:
@@ -82,7 +100,7 @@ def create_CSR(key):
     # Sign the CSR with our private key
     ).sign(key, hashes.SHA256())
     # write the CSR to disk
-    with open("user.pem", "wb") as f:
+    with open("user.csr", "wb") as f:
         f.write(csr.public_bytes(serialization.Encoding.PEM))
 
 
@@ -128,3 +146,76 @@ def create_self_signed_certificate(key):
     with open("root_certificate.pem", "wb") as f:
         f.write(cert.public_bytes(serialization.Encoding.PEM))
 
+
+def load_csr_and_issue_certificate(key, cert, csr):
+    x509_ca_cert = x509.load_pem_x509_certificate(cert)
+
+    x509_csr = x509.load_pem_x509_csr(csr)
+    if x509_csr.is_signature_valid:
+        print("CSR signature is valid!!!")
+    else:
+        print("CSR signature is invalid!!!")
+        return False
+
+    s_cn = x509_csr.subject.get_attributes_for_oid(NameOID.COUNTRY_NAME)[0].value
+    s_st = x509_csr.subject.get_attributes_for_oid(NameOID.STATE_OR_PROVINCE_NAME)[0].value
+    s_ln = x509_csr.subject.get_attributes_for_oid(NameOID.LOCALITY_NAME)[0].value
+    s_on = x509_csr.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value
+    s_c = x509_csr.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+    s_publickey = x509_csr.public_key()
+
+    i_cn = x509_ca_cert.subject.get_attributes_for_oid(NameOID.COUNTRY_NAME)[0].value
+    i_st = x509_ca_cert.subject.get_attributes_for_oid(NameOID.STATE_OR_PROVINCE_NAME)[0].value
+    i_ln = x509_ca_cert.subject.get_attributes_for_oid(NameOID.LOCALITY_NAME)[0].value
+    i_on = x509_ca_cert.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value
+    i_c = x509_ca_cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+
+    print("CSR information")
+    print("Country Name: " + s_cn)
+    print("State or Province Name: " + s_st)
+    print("Locality Name: " + s_ln)
+    print("Organization Name: " + s_on)
+    print("Common Name: " + s_c)
+
+    subject = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, s_cn),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, s_st),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, s_ln),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, s_on),
+        x509.NameAttribute(NameOID.COMMON_NAME, s_c),
+    ])
+
+    issuer = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, i_cn),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, i_st),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, i_ln),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, i_on),
+        x509.NameAttribute(NameOID.COMMON_NAME, i_c),
+    ])
+
+    cert = x509.CertificateBuilder().subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        s_publickey
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        # 1 year in duration
+        datetime.datetime.utcnow() + datetime.timedelta(days=365)
+    ).add_extension(
+        x509.KeyUsage(digital_signature=True, key_encipherment=True, key_cert_sign=False, crl_sign=False,
+                      content_commitment=False, data_encipherment=True, key_agreement=True, encipher_only=False,
+                      decipher_only=False),
+        critical=True
+        # Sign the certificate
+    ).sign(key, hashes.SHA256())
+    # write certificate to disk
+    with open("user.crt", "wb") as f:
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
+
+    return True
